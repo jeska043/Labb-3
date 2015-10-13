5ecb0dbc39fa31d1b5204bd71b248c9f30736bb5
jeska043 <jeska043@student.liu.se>
2015-10-13 11:59:00 +0200

    Börjat på Variable_Table

---
+++ Variable_Table/Variable_Table.cc
@@ -0,0 +1,49 @@
+#include "Variable_Table.h"
+#include <string>
+#include <map>
+using namespace std;
+
+void Variable_Table::insert(const string& name, long double value)
+{
+    variable_table.insert(pair<string,long double>(name,value));
+}
+
+void Variable_Table::remove(const string& name)
+{
+    variable_table.erase(name);
+}
+
+bool Variable_Table::find(const string& name)
+{
+    return (! (variable_table.count(name) == 0));
+}
+
+void Variable_Table::set_value(const string& name, long double value)
+{
+    variable_table.find(name)->second = value; 
+}
+
+long double Variable_Table::get_value(const string& name)
+{
+    long double answer{variable_table.find(name)->second};
+    return answer;
+}
+
+void Variable_Table::list(ostream& os)
+{
+    for(map<string,long double>::iterator it{variable_table.begin()};
+        it != variable_table.end(); ++it)
+        os << it->first << ": " << it->second << endl;
+}
+
+void Variable_Table::clear()
+{
+    for(map<string,long double>::iterator it{variable_table.begin()};
+        it != variable_table.end(); ++it)
+        variable_table.erase(it);
+}
+
+bool Variable_Table::empty()
+{
+    return variable_table.empty();
+} 
---
+++ Variable_Table/variable_table-test.cc
@@ -0,0 +1,13 @@
+#include "Variable_Table.h"
+#include <string>
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+Variable_Table VT1;
+VT1.insert("x",5);
+
+return 0;
+}
